4e95c5004c9b23423d96177219e7ee116d09acab
OSMAN ESKILIOGLU <osmaneskilioglu@gmail.com>
2021-01-19 23:40:10 +0300

    Add project files.

---
+++ Out_Example1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Out_Example1
+{
+	class OutParametre
+	{
+		int uc, bes = 0;
+		// Üçe veya beşe bölünen sayıları adetini bulan metot 
+		// out sayesinde sayi parametresinin değeri de geriye döner
+		public int GetSayi(int n, out int sayi)
+		{
+
+			for (int i = 1; i <= n; i++)
+			{
+
+				if (i % 3 == 0)
+					uc++;
+
+				if (i % 5 == 0)
+					bes++;
+			}
+			sayi = bes;
+
+			// integer kısmı geriye döndürür
+			return uc;
+		}
+	}
+
+	class OutOrnek
+	{
+		static void Main()
+		{
+			OutParametre op = new OutParametre();
+			int a, b;
+
+			// İki değer döndürmek için ikinci argümanı out ile gönderiyoruz
+			// b değişkeni GetSayi metodunda sayi değişkeninin değerlerini taşır
+			a = op.GetSayi(25, out b);
+
+			System.Console.WriteLine("Üçe bölünen sayısı: " + a);
+			System.Console.WriteLine("Beşe bölünen sayısı: " + b);
+		}
+	}
+}
+		
+
---
+++ Ref_example1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ref_example1
+{
+    class Referans
+    {
+        // Argümanı değiştiren metot
+        // ref'in kullanımı önemli
+        public void Kare(ref int i, ref int k)
+        {
+            i = i * k;
+        }
+    }
+
+    class RefOrnek
+    {
+        static void Main()
+        {
+            Referans rf = new Referans();
+
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("Çağrıdan önce: " + a);
+
+            // ref'in kullanımı önemli 
+            rf.Kare(ref a, ref b);
+
+            Console.WriteLine("Çağrıdan sonra: " + a);
+        }
+    }
+
+}
